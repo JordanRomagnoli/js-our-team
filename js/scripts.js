68cc1aaef1aba5043ce8fa0c2ca35529cf34139f
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-18 14:28:54 +0100

    - CREATE function for create user profile; - CREATE array w/ all Profile; - ADD cycle for who stamp all key and values of profile Object

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,56 @@
-// Test funzionamento
-console.log('Template');
+const profileList = [
+    createNewProfile('Wayne Barnett', 'Founder & CEO', 'img/wayne-barnett-founder-ceo.jpg'),
+    createNewProfile('Angela Caroll', 'Chief Editor', 'img/angela-caroll-chief-editor.jpg'),
+    createNewProfile('Walter Gordon', 'Office Manager', 'img/walter-gordon-office-manager.jpg'),
+    createNewProfile('Angela Lopez', 'Social Media Manager', 'img/angela-lopez-social-media-manager.jpg'),
+    createNewProfile('Scott Estrada', 'Developer', 'img/scott-estrada-developer.jpg'),
+    createNewProfile('Barbara Ramos', 'Graphic Designer', 'img/barbara-ramos-graphic-designer.jpg')
+]; 
+
+
+/* for(let i = 0; i < profileList.length; i++){
+
+    console.log(profileList[i].name);
+    console.log(profileList[i].role);
+    console.log(profileList[i].immage);
+} */
+
+for(let i = 0; i < profileList.length; i++){
+
+    for(let key in profileList[i]){
+
+        console.log(key+': ', profileList[i][key]);
+        
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---- FUNTIONS --------------------------------------------------------
+
+function createNewProfile(userName, userRole, userImmage){
+    let newProfile = {
+        name : userName,
+        role : userRole,
+        immage : `
+            <img src="${userImmage}">
+        `
+    };
+
+    return newProfile;
+};
